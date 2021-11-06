1323384b770651cd98756ae55323b8c497edde41
Watcahanan <79349006+robot-eng@users.noreply.github.com>
2021-11-06 22:40:14 +0700

    Add files via upload

---
+++ src/robot_base/BTS7960.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+from __future__ import division
+from Adafruit_PCA9685.PCA9685 import PCA9685
+from geometry_msgs.msg import Twist
+import rospy
+import RPi.GPIO as io
+io.setmode(io.BOARD)
+import Adafruit_PCA9685
+import time
+io.setwarnings(False)
+PCA9685_pwm = Adafruit_PCA9685.PCA9685()
+PCA9685_pwm.set_pwm_freq(60)
+duty_cycle = 4095
+#setmotorleft
+leftmotor_int1_pin = 22
+leftmotor_int2_pin = 23
+io.setup(leftmotor_int1_pin, io.OUT)
+io.setup(leftmotor_int2_pin, io.OUT)
+#setmotorright
+rightmotor_int1_pin = 21
+rightmotor_int2_pin = 19
+io.setup(rightmotor_int1_pin, io.OUT)
+io.setup(rightmotor_int2_pin, io.OUT)
+
+io.output(leftmotor_int1_pin, True)
+io.output(leftmotor_int2_pin, True)
+io.output(rightmotor_int1_pin, True)
+io.output(rightmotor_int2_pin, True)
+
+def setMotorMode(motor, mode):
+   if motor == "leftmotor":
+      if mode == "reverse":
+         io.output(leftmotor_int1_pin, True)
+         io.output(leftmotor_int2_pin, False)
+      elif  mode == "forward":
+         io.output(leftmotor_int1_pin, False)
+         io.output(leftmotor_int2_pin, True)
+      else:
+         io.output(leftmotor_int1_pin, False)
+         io.output(leftmotor_int2_pin, False)
+   elif motor == "rightmotor":
+      if mode == "reverse":
+         io.output(rightmotor_int1_pin, False)
+         io.output(rightmotor_int2_pin, True)      
+      elif  mode == "forward":
+         io.output(rightmotor_int1_pin, True)
+         io.output(rightmotor_int2_pin, False)
+      else:
+         io.output(rightmotor_int1_pin, False)
+         io.output(rightmotor_int2_pin, False)
+   else:
+      io.output(leftmotor_int1_pin, False)
+      io.output(leftmotor_int2_pin, False)
+      io.output(rightmotor_int1_pin, False)
+      io.output(rightmotor_int2_pin, False)
+
+def setMotorLeft(power):
+   int(power)
+   if power < 0:
+      pwm = -int(duty_cycle * power)
+      if pwm > duty_cycle:
+         pwm = duty_cycle
+      PCA9685_pwm.set_pwm(0, 0, pwm)
+      PCA9685_pwm.set_pwm(1, 0, 0)   
+   elif power > 0:
+      pwm = int(duty_cycle * power)
+      if pwm > duty_cycle:
+         pwm = duty_cycle
+      PCA9685_pwm.set_pwm(0, 0, 0)
+      PCA9685_pwm.set_pwm(1, 0, pwm)  	  
+   else:
+      PCA9685_pwm.set_pwm(0, 0, 0)
+      PCA9685_pwm.set_pwm(1, 0, 0)  
+      PCA9685_pwm.set_pwm(2, 0, 0)
+      PCA9685_pwm.set_pwm(3, 0, 0)	
+
+def setMotorRight(power):
+   int(power)
+   if power < 0:
+      pwm = -int(duty_cycle * power)
+      if pwm > duty_cycle:
+         pwm = duty_cycle
+      PCA9685_pwm.set_pwm(2, 0, pwm)
+      PCA9685_pwm.set_pwm(3, 0, 0)  	  
+   elif power > 0:
+      pwm = int(duty_cycle * power)
+      if pwm > duty_cycle:
+         pwm = duty_cycle
+      PCA9685_pwm.set_pwm(2, 0, 0)
+      PCA9685_pwm.set_pwm(3, 0, pwm)  	  
+   else:
+      PCA9685_pwm.set_pwm(0, 0, 0)
+      PCA9685_pwm.set_pwm(1, 0, 0)  
+      PCA9685_pwm.set_pwm(2, 0, 0)
+      PCA9685_pwm.set_pwm(3, 0, 0)
+
+def exit():
+   io.output(leftmotor_int1_pin, False)
+   io.output(leftmotor_int2_pin, False)
+   io.output(rightmotor_int1_pin, False)
+   io.output(rightmotor_int2_pin, False)
+   io.cleanup()
+   PCA9685_pwm.set_pwm(0, 0, 0)
+   PCA9685_pwm.set_pwm(1, 0, 0)
+   PCA9685_pwm.set_pwm(2, 0, 0)
+   PCA9685_pwm.set_pwm(3, 0, 0)
+
+
+def setMotor(msg):
+   setMotorLeft(msg.linear.x)
+   setMotorRight(msg.linear.y)
+   rospy.loginfo(msg.linear.x)
+   rospy.loginfo(msg.linear.y)
+
+
+if __name__ == '__main__':
+   print ("Starting motor node")
+   rospy.init_node('motors')
+   #rospy.Subscriber('cmd_vel', String, queue_size=10)
+   rospy.Subscriber('cmd_vel', Twist, setMotor)
+   rospy.spin()
+
---
+++ src/robot_base/RobotControl.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+import sys, tty, termios, os, readchar
+from geometry_msgs.msg import Twist
+#import BTS7960 as #HBridge
+import rospy
+
+speedleft = 0
+speedright = 0
+print("w/s: forward/back")
+print("a/d: left/right")
+print("q: stoppt Motor")
+print("x: Programm end")
+
+def getch():
+   ch = readchar.readchar()
+   return ch
+def printscreen():
+   print("========== MotorSet ==========")
+   print ("Motor Speedleft:  ", speedleft)
+   print ("Motor Speedright: ", speedright)
+
+if __name__ == '__main__':
+   pub = rospy.Publisher('cmd_vel', Twist, queue_size=0)
+   rospy.init_node('Control_motor', anonymous=True)
+   #rospy.Subscriber('Control', String, queue_size=10)
+   speed = Twist()
+
+   while not rospy.is_shutdown():
+
+      char = getch()
+      if(char == "w"):
+
+         speedleft = speedleft + 0.1
+         speedright = speedright + 0.1
+
+         if speedleft > 1:
+            speedleft = 1
+         if speedright > 1:
+            speedright = 1
+         ##HBridge.setMotorLeft(speedleft)
+         ##HBridge.setMotorRight(speedright)
+         printscreen()
+         
+      if(char == "s"):
+
+         speedleft = speedleft - 0.1
+         speedright = speedright - 0.1
+
+         if speedleft < -1:
+            speedleft = -1
+         if speedright < -1:
+            speedright = -1
+      
+         ##HBridge.setMotorLeft(speedleft)
+         ##HBridge.setMotorRight(speedright)
+         printscreen()
+
+      if(char == "q"):
+         speedleft = 0
+         speedright = 0
+         ##HBridge.setMotorLeft(0)
+         ##HBridge.setMotorRight(0)
+         printscreen()
+
+      if(char == "d"):      
+         speedright = speedright - 0.1
+         speedleft = speedleft + 0.1
+      
+         if speedright < -1:
+            speedright = -1
+      
+         if speedleft > 1:
+            speedleft = 1
+      
+         ##HBridge.setMotorLeft(speedleft)
+         ##HBridge.setMotorRight(speedright)
+         printscreen()
+
+      if(char == "a"):
+         speedleft = speedleft - 0.1
+         speedright = speedright + 0.1
+         
+         if speedleft < -1:
+            speedleft = -1
+      
+         if speedright > 1:
+            speedright = 1
+      
+         #HBridge.setMotorLeft(speedleft)
+         #HBridge.setMotorRight(speedright)
+      
+         printscreen()
+
+      if(char == "x"):
+         speedleft = 0
+         speedright = 0
+         print ("Program Ended motorset 0")
+         rospy.loginfo_once("ctrl+c exit")
+         break
+
+      speed.linear.x = speedleft
+      speed.linear.y = speedright
+ 
+      char = ""
+      
+      pub.publish(speed)
+ 
