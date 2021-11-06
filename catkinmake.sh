90af9894468881ca341dd101694fc21e99cf64ae
Watcahanan <79349006+robot-eng@users.noreply.github.com>
2021-11-06 22:51:54 +0700

    Update catkinmake.sh

--- catkinmake.sh
+++ catkinmake.sh
@@ -1,4 +1,6 @@
 catkin_make
-echo "source ~/jetson_robot/devel/setup.bash" >> ~./bashrc
+echo "source ~/jetson_robot/devel/setup.bash" >> /home/aum/.bashrc 
 source ~/.bashrc
 echo "Catkin_make Finish"
+cd ~/jetson_robot/src/robot_base
+chmod +x *
